9a53fad18065b1ca5a953dfa73e199462907c783
yehric2018 <eric.yehj@outlook.com>
2022-02-27 02:11:21 +0000

    Moved test dataset to tests dir

--- OSWValidation/validator.py
+++ OSWValidation/validator.py
@@ -4,8 +4,8 @@ import json
 from . import json_structure
 
 def validate(geojson):
-    print(os.getcwd())
-    with open(os.path.join(sys.path[0], 'OSWValidation/Json Schema/Ways_schema.json'), 'r') as fp:
+    schema_filename = 'OSWValidation/Json Schema/Ways_schema.json'
+    with open(os.path.join(sys.path[0], schema_filename), 'r') as fp:
         schema = json.load(fp)
 
     invalid_schema_ways_dict = json_structure.validate_json_structure(geojson, schema)
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,4 +1,11 @@
+import os
+import sys
+import json
 from OSWValidation import validator
 
-def test_validate_json_structure():
-    assert validator.validate({}) == 1
+def test_validate_entire_dataset():
+    test_dataset_filename = 'tests/TestData/valparaiso/input/valparaiso_ways.geojson'
+
+    with open(os.path.join(sys.path[0], test_dataset_filename), 'r') as fp:
+        geojson = json.load(fp)
+    assert validator.validate(geojson) == 1
